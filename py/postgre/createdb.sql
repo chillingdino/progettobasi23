62ab889dffc66d4ca3c2e5a6d6647ce1b02472f0
chillingdino <84202194+chillingdino@users.noreply.github.com>
2023-09-18 16:29:43 +0100

    ultimi cambiamenti sql

--- py/postgre/createdb.sql
+++ py/postgre/createdb.sql
@@ -31,13 +31,15 @@ CREATE TABLE Esami(
 	PRIMARY KEY (codEsame)
 );
 
-
-
 CREATE TABLE Prove(
     codProva varchar(50) not null,
 	nomeProva varchar(50) not null, 
   	esame varchar(50) not null,
     docenteReferente varchar(50),
+
+    descrizione varchar(50),
+    durata varchar(50),
+    
     tipoProva varchar(50),
     dataProva timestamp not null,
     dataScandenza timestamp not null,
@@ -65,72 +67,9 @@ CREATE TABLE Risulato_prove(
 );
 
 CREATE TABLE Iscrizione_prove(
-	risultato int, --perche ce questo boh? 
 	prova varchar(50),
 	studente varchar(50),
   	PRIMARY KEY (prova,studente),
 	FOREIGN KEY (prova) REFERENCES Prove(codProva),
 	FOREIGN KEY (studente) REFERENCES Utenti(codFiscale)
 );
-
-
---trigger che impedisce iscrizione a prova se studente non ha >18 voto su richiestaprova
-CREATE TRIGGER prevent_iscrizione_prova_requisiti
-BEFORE INSERT ON Iscrizione_prove
-FOR EACH ROW
-BEGIN
-    -- Verifica se richestoSuperamentoCodProva non è NULL
-    IF EXISTS (SELECT 1 FROM Prove p WHERE p.codProva = NEW.prova AND p.richestoSuperamentoCodProva IS NOT NULL) THEN
-        -- Verifica se lo studente ha un voto > 18 nella prova richiesta
-        IF NOT EXISTS (
-            SELECT 1
-            FROM Risultato_prove rp
-            WHERE rp.esame = (
-                SELECT richestoSuperamentoCodProva
-                FROM Prove
-                WHERE codProva = NEW.prova
-            ) AND rp.studente = NEW.studente AND rp.voto > 18
-        ) THEN
-            return null
-            SET MESSAGE_TEXT = 'Non è possibile iscriversi alla prova, requisiti non soddisfatti.';
-        END IF;
-    END IF;
-END;
-
-
-CREATE TRIGGER ControlloCreazioneEsame
-BEFORE INSERT ON Prove
-FOR EACH ROW
-WHEN (NOT EXISTS SELECT * 
-		FROM Esami
-		WHERE new.prova=Esami.codEsame and (new.docenteReferente=Esami.docente OR new.docenteReferente=Esami.docenteReferente)
-BEGIN 
-RETURN NULL
-END;
-
---setta a 0 il risultato della prova passata nel caso in cui lo studente si iscriva nuovamente
-CREATE TRIGGER invalidaProva
-AFTER INSERT ON Iscrizione_prove
-FOR EACH ROW
-BEGIN
-    DECLARE prova_nome VARCHAR(50);
-    DECLARE existing_voto INT;
-
-    -- Ottieni il nome della prova per il nuovo record inserito in Iscrizione_prove
-    SELECT nomeProva INTO prova_nome
-    FROM Prove
-    WHERE codProva = NEW.prova;
-
-    -- Controlla se l'utente ha già svolto la prova in passato
-    SELECT voto INTO existing_voto
-    FROM Risultato_prove rp
-    JOIN Prove p ON rp.esame = p.esame
-    WHERE p.nomeProva = prova_nome AND rp.studente = NEW.studente;
-
-    -- Se l'utente ha già svolto la prova in passato, imposta a 0 il voto
-    IF existing_voto IS NOT NULL THEN
-        UPDATE Risultato_prove
-        SET voto = 0
-        WHERE esame IN (SELECT codEsame FROM Prove WHERE nomeProva = prova_nome) AND studente = NEW.studente;
-    END IF;
-END;
--- py/postgre/insert.sql
+++ py/postgre/insert.sql
@@ -1,15 +1,14 @@
 INSERT INTO Utenti VALUES ('admin', 'admin','admin',25,0,'0','admin','$2b$10$aTaqNiv5vBc1SdkscE0TT.cKk9/sQESmt/W2PoZ8vREQ501aA8DiG','admin');
 INSERT INTO Utenti VALUES ('codMario', 'mario','rossi',26,0,'0','emailMario','$2b$10$aTaqNiv5vBc1SdkscE0TT.cKk9/sQESmt/W2PoZ8vREQ501aA8DiG','utente');
-INSERT INTO Utenti VALUES ('codMaria', 'maria','rossi',35,1,'0','emailMaria','$2b$10$aTaqNiv5vBc1SdkscE0TT.cKk9/sQESmt/W2PoZ8vREQ501aA8DiG','utente');
 
 INSERT INTO Utenti (codFiscale, nome, cognome, eta, sesso, cellulare, email, password, ruolo)
 VALUES
     ('CF001', 'Mario', 'Rossi', 25, 1, '+123456789', 'mario@email.com', '$2b$10$aTaqNiv5vBc1SdkscE0TT.cKk9/sQESmt/W2PoZ8vREQ501aA8DiG', 'utente'),
     ('CF002', 'Laura', 'Bianchi', 22, 0, '+987654321', 'laura@email.com', '$2b$10$aTaqNiv5vBc1SdkscE0TT.cKk9/sQESmt/W2PoZ8vREQ501aA8DiG', 'utente'),
     ('CF003', 'Luigi', 'Verdi', 28, 1, '+555555555', 'luigi@email.com', '$2b$10$aTaqNiv5vBc1SdkscE0TT.cKk9/sQESmt/W2PoZ8vREQ501aA8DiG', 'professore'),
-    ('CF004', 'Anna', 'Gialli', 20, 0, '+111111111', 'anna@email.com', '$2b$10$aTaqNiv5vBc1SdkscE0TT.cKk9/sQESmt/W2PoZ8vREQ501aA8DiG', 'utente'),
-    ('CF005', 'Roberto', 'Neri', 30, 1, '+999999999', 'roberto@email.com', '$2b$10$aTaqNiv5vBc1SdkscE0TT.cKk9/sQESmt/W2PoZ8vREQ501aA8DiG', 'admin');
-
+    ('CF004', 'Marco', 'Rossi', 28, 1, '+555555555', 'marco@email.com', '$2b$10$aTaqNiv5vBc1SdkscE0TT.cKk9/sQESmt/W2PoZ8vREQ501aA8DiG', 'professore'),
+    ('CF005', 'Anna', 'Gialli', 20, 0, '+111111111', 'anna@email.com', '$2b$10$aTaqNiv5vBc1SdkscE0TT.cKk9/sQESmt/W2PoZ8vREQ501aA8DiG', 'utente'),
+    
 
 -- Inserimento di esami di prova
 INSERT INTO Esami (codEsame, materia, docenteReferente, docente)
@@ -18,32 +17,31 @@ VALUES
     ('E002', 'Fisica', 'CF003', NULL),
     ('E003', 'Informatica', 'CF003', NULL);
 
+-- Popolare la tabella Prove con dati di esempio
+INSERT INTO Prove (codProva, nomeProva, esame, docenteReferente, descrizione, durata, tipoProva, dataProva, dataScandenza, richestoSuperamentoCodProva, completo)
+VALUES
+    ('P001', 'Esame Finale', 'E001', 'CF003', 'Esame finale di Matematica', '3 ore', 'voto', '2023-09-30 10:00:00', '2023-09-30 13:00:00', NULL, true),
+    ('P002', 'Esame Parziale', 'E002', 'CF004', 'Esame parziale di Fisica', '2 ore', 'voto', '2023-09-22 14:00:00', '2023-09-22 16:00:00', NULL, false);
+    ('P003', 'Esame Finale', 'E002', 'CF004', 'Esame parziale di Fisica', '2 ore', 'voto', '2023-09-22 14:00:00', '2023-09-22 16:00:00', P2, true);
+
 -- Inserimento di esami superati di prova
 INSERT INTO Esami_superati (esame, studente, voto)
 VALUES
     ('E001', 'CF001', 28),
     ('E001', 'CF002', 25),
-    ('E002', 'CF001', 30),
-    ('E002', 'CF002', 27);
-    
--- Inserimento di prove di prova
-INSERT INTO Prove (codProva, nomeProva, esame, docenteReferente, tipoProva, dataProva, dataScandenza, richestoSuperamentoCodProva, completo)
-VALUES
-    ('P001', 'Parziale', 'E001', 'CF003', 'voto', '2023-08-01 10:00:00', '2023-08-01 12:00:00', NULL, true),
-    ('P002', 'Finale', 'E001', 'CF003', 'voto', '2023-08-15 14:00:00', '2023-08-15 16:00:00', 'P001', false),
-    ('P003', 'Pratica', 'E002', 'CF003', 'voto', '2023-08-10 11:00:00', '2023-08-10 13:00:00', NULL, true);
+    ('E001', 'CF001', 30),
+    ('E001', 'CF002', 27);
 
 -- Inserimento di risultati delle prove
 INSERT INTO Risulato_prove (prova, voto, studente)
 VALUES
-    ('P001', 'voto', 'CF001'),
-    ('P002', 'voto', 'CF002'),
-    ('P003', 'voto', 'CF003');
-
+    ('P002', '10', 'CF001'),
+    ('P002', '30', 'CF002'),
+    ('P002', '20', 'CF003');
 
 -- Inserimento di iscrizioni a prove di prova
-INSERT INTO Iscrizione_prove (risultato, prova, studente)
+INSERT INTO Iscrizione_prove (prova, studente)
 VALUES
-    (NULL, 'P001', 'CF001'),
-    (NULL, 'P002', 'CF002'),
-    (NULL, 'P003', 'CF003');
+    ('P001', 'CF001'),
+    ('P002', 'CF002'),
+    ( 'P003', 'CF003');
